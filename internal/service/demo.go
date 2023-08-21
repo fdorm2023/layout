7b5d8ac60c51ebba89a022b3ae65e967e51fd8dc
vanle <vank48dhv@gmail.com>
2023-08-21 21:39:20 +0700

    init

--- internal/service/demo.go
+++ internal/service/demo.go
@@ -3,6 +3,8 @@ package service
 import (
 	"context"
 	"f-dorm/app/demo/internal/biz"
+	"f-dorm/core/const/codename"
+	"f-dorm/core/logger"
 
 	pb "f-dorm/api/demo/v1"
 )
@@ -20,6 +22,7 @@ func NewDemoService(uc *biz.GreeterUsecase) *DemoService {
 }
 
 func (s *DemoService) SayHello(ctx context.Context, req *pb.SayHelloRequest) (*pb.SayHelloReply, error) {
+	ctx = logger.NewContextWithTraceID(ctx, codename.ServiceDemo+"-SayHello")
 	message, err := s.uc.GenerateHelloMessage(ctx, req.UserName)
 	if err != nil {
 		return nil, err
